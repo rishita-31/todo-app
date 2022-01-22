260f7366613fe2a6a6fd6adf919b196f7e8fc31b
rishita-31 <84236626+rishita-31@users.noreply.github.com>
2022-01-22 19:42:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import './App.css';
+import Header from "./MyComponents/Header";
+import {Todos} from "./MyComponents/Todos";
+import {Footer} from "./MyComponents/Footer";
+import React, {useState, useEffect} from 'react';
+import {AddTodo} from "./MyComponents/AddTodo";
+import {About} from "./MyComponents/About";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+function App() {
+  let initTodo;
+  if(localStorage.getItem("todos")===null){
+    initTodo =[];
+  }else{
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+
+  const onDelete = (todo)=>{
+    console.log("I am onDelete of todo", todo);
+
+    setTodos(todos.filter((e)=>{
+      return e!==todo;
+    }));//setTodos is a func to update todos
+    localStorage.setItem("todos",JSON.stringify(todos));
+  }
+
+  const addTodo = (title,desc)=>{
+    console.log("I am adding this todo",title,desc)
+
+    let sno;
+    if(todos.length===0){
+      sno = 0;
+    }else{
+      sno = todos[todos.length - 1].sno + 1;
+    }
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc
+    }
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+  }
+
+  const [todos, setTodos] = useState(initTodo);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
+
+  return (
+    <>
+      <Router>
+        <Header title="MyTodosApp" searchBar={true} />
+
+        <Switch>
+          <Route exact path="/" render={()=>{
+            return(
+              <>
+              <AddTodo addTodo={addTodo} />
+              <Todos todos = {todos} onDelete={onDelete} />
+              </>
+            )
+            }}>
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </Router>
+    </>
+  );
+}
+
+export default App;
